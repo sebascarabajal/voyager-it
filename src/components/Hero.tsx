c74111ecada21c6cc7a383cd0a3d7599e8a17b37
sebascarabajal <sebasfmt@gmail.com>
2024-04-27 18:13:54 -0300

    fix hero type

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ import { useTypewriter } from 'react-simple-typewriter'
 function Hero() {
 
     const [text] = useTypewriter({
-        words: ['the future.', 'the best IT solutions.', 'the best IT services.', 'the best IT support.', 'the best IT experience.', 'the best IT company.', 'the best IT team.', 'the best IT experts.', 'the best IT professionals.'],
+        words: ['future.', 'IT solutions.', 'IT services.', 'IT support.', 'IT experience.', 'IT company.', 'IT team.', 'IT experts.', 'IT professionals.'],
         loop: false,
         deleteSpeed: 70,
         typeSpeed: 100, // Optional
@@ -17,7 +17,7 @@ function Hero() {
             <div className="max-w-md w-full space-y-8">
                 <div>
                     <h2 className="mt-8 mb-4 py-4 text-center text-5xl font-extrabold bg-gradient-to-r from-blue-600 via-green-500 to-indigo-400 text-transparent bg-clip-text">Voyager IT</h2>
-                    <p className="mt-2 text-center text-2xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">Welcome to {text}</p>
+                    <p className="mt-2 text-center text-2xl bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text">Welcome to the best {text}</p>
                     <div className="flex justify-center mt-6">
                         <ButtonHero />
                     </div>
