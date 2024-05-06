e0901eb00f16b19ff0f85db4efbb2fe144de7a73
sebascarabajal <sebasfmt@gmail.com>
2024-05-06 13:49:40 -0300

    finish two

--- src/app/projects/Ionixx/Images/page.tsx
+++ src/app/projects/Ionixx/Images/page.tsx
@@ -6,8 +6,8 @@ export default function Images() {
         <main className='min-h-screen'>
             <div className='flex flex-col items-center justify-center'>
             <h2 className="text-2xl font-bold bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-transparent bg-clip-text mt-20 mb-10">Planos en 2D del cableado estructurado.</h2>
-                <Image src={"/PlantaBaja.png"} width={1000} height={600} alt='Plano de la planta baja.' />
-                <Image src={"/PlantaAlta.png"} width={1000} height={600} alt='Plano de la planta alta.' />
+                <Image src={"/baja.png"} width={1000} height={600} alt='Plano de la planta baja.' />
+                <Image src={"/alta.png"} width={1000} height={600} alt='Plano de la planta alta.' />
             </div>
         </main>
     )
