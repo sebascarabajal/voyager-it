215c63223a27bd2c4fe61e70dd66da7dde0e7509
sebascarabajal <sebasfmt@gmail.com>
2024-05-06 18:02:31 -0300

    finish three

--- src/app/projects/Ionixx/Introduccion/page.tsx
+++ src/app/projects/Ionixx/Introduccion/page.tsx
@@ -8,12 +8,12 @@ export default function Intro() {
                 <div className="max-w-screen-md mx-4">
                     Este documento presenta una exhaustiva gu&iacute;a sobre el proyecto de cableado estructurado diseñado para satisfacer las necesidades espec&iacute;ficas de nuestra empresa. Abarcando desde la selecci&oacute;n de materiales hasta la implementaci&oacute;n de tecnolog&iacute;a de vanguardia, este proyecto se centra en proporcionar una infraestructura de red robusta y adaptable que respalde las operaciones presentes y futuras de la organizaci&oacute;n.
 
-                    Exploraremos detalladamente los materiales seleccionados para este proyecto, destacando su calidad, durabilidad y capacidad para soportar las demandas de un entorno empresarial din&aacute;mico. Adem&aacute;s, se analizar&aacuten las &uacute;ltimas tendencias y avances en tecnolog&iacute;a de cableado estructurado, con el objetivo de garantizar la incorporaci&oacute;n de soluciones innovadoras que impulsen la eficiencia y la productividad.
+                    Exploraremos detalladamente los materiales seleccionados para este proyecto, destacando su calidad, durabilidad y capacidad para soportar las demandas de un entorno empresarial din&aacute;mico. Adem&aacute;s, se analizar&aacute;n las &uacute;ltimas tendencias y avances en tecnolog&iacute;a de cableado estructurado, con el objetivo de garantizar la incorporaci&oacute;n de soluciones innovadoras que impulsen la eficiencia y la productividad.
 
                     El diseño del cableado estructurado desempeña un papel crucial en la optimizaci&oacute;n del flujo de datos y la maximizaci&oacute;n del rendimiento de la red. Por lo tanto, este documento tambi&eacute;n examina en profundidad el diseño propuesto, teniendo en cuenta aspectos como la topología de la red, la distribución de los puntos de conexi&oacute;n y la gesti&oacute;n de la energ&iacute;a, entre otros.
                 </div>
                 <div className="max-w-screen-md mx-4">
-                    Aqui debajo te dejamos el bot&oacute;n de descarga para que puedas acceder a la documentaci&oacute;n completa en PDF.
+                    A continuaci&oiacute; te dejamos el bot&oacute;n de descarga para que puedas acceder a la documentaci&oacute;n completa en PDF.
                 </div>
                 <a href="/VoyagerTP1.pdf" download className="bg-teal-500 hover:bg-teal-700 text-white font-bold py-2 px-4 rounded mt-6 mb-6">Descargar PDF</a>
             </div>
